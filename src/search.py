c8013aced559ada1c4483c51c4ef490d1f12d475
Michael Martin <michael.j.martin682@gmail.com>
2024-03-28 17:14:32 -0400

    intial commit

---
+++ src/search.py
@@ -0,0 +1,67 @@
+import time
+
+import torch
+import numpy as np
+from torchvision import models, transforms
+
+import cv2
+from PIL import Image
+
+import json
+
+def checkParking():
+    torch.backends.quantized.engine = 'qnnpack'
+
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 224)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 224)
+    cap.set(cv2.CAP_PROP_FPS, 36)
+
+    preprocess = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    net = models.quantization.mobilenet_v2(pretrained=True, quantize=True)
+    # jit model to take it from ~20fps to ~30fps
+    net = torch.jit.script(net)
+
+    started = time.time()
+    last_logged = time.time()
+    frame_count = 0
+
+    # import class labels
+    classes = {}
+    with open('../data/imagenet1000_clsidx_to_labels.json') as f:
+        classes = json.load(f)
+
+    with torch.no_grad():
+        # read frame
+        ret, image = cap.read()
+        if not ret:
+            raise RuntimeError("failed to read frame")
+
+        # convert opencv output from BGR to RGB
+        image = image[:, :, [2, 1, 0]]
+        permuted = image
+
+        # preprocess
+        input_tensor = preprocess(image)
+
+        # create a mini-batch as expected by the model
+        input_batch = input_tensor.unsqueeze(0)
+
+        # run model
+        output = net(input_batch)
+        # do something with output ...
+
+        top = list(enumerate(output[0].softmax(dim=0)))
+        top.sort(key=lambda x: x[1], reverse=True)
+
+        idx = top[:1][0][0]
+        p = top[:1][0][1] * 100
+        return classes[str(idx)]
+
+
+        # for idx, val in top[:10]:
+        #     print(f"{val.item()*100:.2f}% {classes[str(idx)]}")
